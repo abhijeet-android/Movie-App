6946691a0d280910c6083ce1a2a766ca70900f92
Abhijeet <abhijeet.aj91@gmail.com>
2021-06-15 02:08:16 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+include(
+    ":app",
+    "core",
+    "lib:commons",
+    "lib:navigation",
+    ":lib:resources",
+    ":lib:testcommons",
+    ":feature:movies",
+    ":feature:moviedetail"
+)
+rootProject.name = "Movie App"
